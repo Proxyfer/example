03eb0a56f8b0078c0780d32fc41e72b1bb1c45f2
Proxyfer <ganjablin@gmail.com>
2015-07-23 16:41:20 +0300

    lets go home

--- www/lesson1.php
+++ www/lesson1.php
@@ -1,5 +1,8 @@
 <html>
-<title>Делаем первое задание</title>
+<head>
+	<title>Делаем первое задание</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
 <body>
 <center>
 <form method="post" action="<?=$_SERVER['PHP_SELF']?>">
@@ -26,11 +29,23 @@ if((isset($_POST['stroke'])) and ($_POST['sumOK']))
 			$sum+=$cifer[$i];
 
 		echo "<br>".$sum;
+
 	}
 
 
 ?>
 </center>
 </form>
+<p class="abzac">string1</p>
+<p>string2</p>
+<h3>are you my father?</h3>
+<h2>divniy1</h2>
+<h1><p>divniy2</p></h1>
+<div class="father">
+	<a class="abzac" href="http://google.com">google</a>
+	<a href="nahui.ru">Тебе туда</a>
+	<p class="abzac">trololo</p>
+	<h3>inside container</h3>
+</div>
 </body>
 </html>
